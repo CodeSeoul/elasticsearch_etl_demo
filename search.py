713526e86925a6650f7fe42f3374b3fac1791ca7
TTS기술담당 John berrybryan <skt.1902654@sk.com>
2017-08-02 16:15:23 +0900

    happy with this now. several examples. feeling good.

--- search.py
+++ search.py
@@ -35,24 +35,66 @@ es = Elasticsearch()
 #######  FIND JAVA ARMOR #######
 ##########################
 
+#results = es.search(index='github', body={
+#    'query': {
+#        'bool': {
+#            'must': [
+#                { 'match': { 'contents': 'armor' } }
+#            ],
+#            'filter': {
+#                'match': { 'file_extension': '.java' }
+#            }
+#        }
+#    },
+#    '_source': ['contents'],
+#    'sort': { '_score': 'desc' },
+#    'size': 2
+#})
+
+##########################
+#######  COUNT FILES BY TYPE #######
+##########################
+
+#results = es.search(index='github', body={
+#    'size': 0,
+#    'aggs': {
+#        'group_by_type': {
+#            'terms': {
+#                'field': 'file_extension.keyword'
+#            },
+#        }
+#    }
+#})
+
+##########################
+#######  AVG SIZE BY TYPE #######
+##########################
+
 results = es.search(index='github', body={
-    'query': {
-        'bool': {
-            'must': [
-                { 'match': { 'contents': 'armor' } }
-            ],
-            'filter': {
-                'match': { 'file_extension': '.java' }
+    'size': 0,
+    'aggs': {
+        'group_by_type': {
+            'terms': {
+                'field': 'file_extension.keyword'
+            },
+            'aggs': {
+                'average_size': {
+                    'avg': {
+                        'field': 'size'
+                    }
+                }
             }
         }
-    },
-    '_source': ['contents'],
-    'sort': { '_score': 'desc' },
-    'size': 2
+    }
 })
 
 
-for result in results['hits']['hits']:
-    printer.pprint(result) 
+
+if len(results['hits']['hits']) > 0:
+    for result in results['hits']['hits']:
+        printer.pprint(result) 
+
+if 'aggregations' in results:
+    printer.pprint(results['aggregations'])
 
 
