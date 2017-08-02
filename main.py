b7702e6b18a11cba79d8ea393a6ad244905d22d4
TTS기술담당 John berrybryan <skt.1902654@sk.com>
2017-08-02 15:03:19 +0900

    Added config template. Main now parses files pretty well.

---
+++ main.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import requests
+
+from elasticsearch import Elasticsearch
+from pprint import PrettyPrinter
+from configparser import ConfigParser
+from os.path import splitext
+
+printer = PrettyPrinter(indent=2)
+
+ACCEPTABLE_FILE_TYPES = ('java', 'py', 'rb', 'js', 'go')
+
+def main():
+    config = ConfigParser()
+    config.read('default.conf')
+    
+    gh_client_id = config['DEFAULT']['client_id']
+    gh_client_secret = config['DEFAULT']['client_secret']
+    
+    headers = {
+                'User-Agent': 'ElasticsearchETLDemo/1.0',
+                'Accept': 'application/vnd.github.v3+json'
+            }
+
+    oauth_string = 'client_id=' + gh_client_id + '&client_secret=' + gh_client_secret
+    
+    result = requests.get('https://api.github.com/users/TheBeege/repos?' + oauth_string, headers=headers).json()
+    #printer.pprint(result)
+    
+    repos_to_consume = [repo['name'] for repo in result ]
+    #print('repos_to_consume:')
+    #printer.pprint(repos_to_consume)
+    
+    es = Elasticsearch()
+    
+    for repo in repos_to_consume:
+        current_contents_path = 'https://api.github.com/repos/TheBeege/' + repo + '/contents/?'
+        
+        result = requests.get(current_contents_path + oauth_string).json()
+        #printer.pprint(result.json())
+        while len(result) > 0:
+            item = result.pop()
+            print('item name:', item['name'])
+            create_document_from_item(repo, item)
+            if item['type'] == 'dir':
+                url = item['_links']['self'] + '&' + oauth_string
+                #print('url:', url)
+                dir_contents = requests.get(url).json()
+                #printer.pprint(dir_contents)
+                result.extend(dir_contents)
+            
+
+def create_document_from_item(repo, item):
+    if item['type'] == 'file' and splitext(item['name'])[1] in ACCEPTABLE_FILE_TYPES:
+        print(requests.get(item['download_url']).text)
+
+        document = {
+                    'name': item['name'],
+                    'url': item['download_url'],
+                    'user': 'TheBeege',
+                    'size': item['size'],
+                    'file_type': splitext(item['name'])[1],
+                    'repo': repo,
+                    'contents': requests.get(item['download_url']).text
+                }
+        printer.pprint(document)
+
+
+if __name__ == '__main__':
+    main()
+
